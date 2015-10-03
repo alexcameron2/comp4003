29c9efb82a28d5631216d42c19058b99962456c1
alexcameron2 <onboardrope@gmail.com>
2015-10-03 16:28:06 -0400

    mandatory message

--- part2.c
+++ part2.c
@@ -3,30 +3,14 @@
 #include <stdio.h>
 #include <string.h>
 
-#include <sqlca.h>
-#include <oraca.h>
-
-EXEC ORACLE OPTION (ORACA=YES);
-EXEC ORACLE OPTION (RELEASE_CURSOR=YES);
-
 void catchOracleError();
 
 int main(){
-	char *username = "fedora";
-	char *password = "oracle11g";
-	char *statement;
-	
-	//If there in an oracle error, handle it
-	EXEC SQL WHENEVER SQLERROR DO catchOracleError();
+	exec sql begin declare section;
+		char *user = "oracle";
+		char pword = "oracle11g";
+	exec sql end declare section;
 
-	//Connect to oracle
-	EXEC SQL CONNECT :username IDENTIFIED BY :password;
-	if (sqlca.sqlcode < 0){
-		printf("Error connecting to oracle!\n");
-		exit(1);
-	}
-	else{
-		printf("Connected to Oracle\n");
-	}
+	printf("Connected to Oracle\n");
 return;	 
 }
