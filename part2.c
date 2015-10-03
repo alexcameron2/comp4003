a5cbb7d90a2b71a5ed101aef23bf448487830006
alexcameron2 <onboardrope@gmail.com>
2015-10-03 16:00:26 -0400

    mandatory message

--- part2.c
+++ part2.c
@@ -1,12 +1,32 @@
 /* This is a dynamic sql program based on an sample written by Alkex Cameron - 100861937 */
 
 #include <stdio.h>
-exec sql include sqlca;
-exec sql begin declare section;
-	char statement[1024];
-	char *MYID = "fedora/oracle11g";
-exec sql end declare section;
+#include <string.h>
+
+#include <sqlca.h>
+#include <oraca.h>
+
+EXEC ORACLE OPTION (ORACA=YES);
+EXEC ORACLE OPTION (RELEASE_CURSOR=YES);
+
+void catchOracleError();
 
 int main(){
+	char *username = "fedora";
+	char *password = "oracle11g";
+	char *statement;
+	
+	//If there in an oracle error, handle it
+	EXEC SQL WHENEVER SQLERROR DO catchOracleError();
 
+	//Connect to oracle
+	EXEC SQL CONNECT :username IDENTIFIED BY :password;
+	if (sqlca.sqlcode < 0){
+		printf("Error connecting to oracle!\n");
+		exit(1);
+	}
+	else{
+		printf("Connected to Oracle\n");
+	}
+return;	 
 }
---
+++ part2.c~
@@ -0,0 +1,12 @@
+/* This is a dynamic sql program based on an sample written by Alkex Cameron - 100861937 */
+
+#include <stdio.h>
+exec sql include sqlca;
+exec sql begin declare section;
+	char statement[1024];
+	char *MYID = "fedora/oracle11g";
+exec sql end declare section;
+
+int main(){
+
+}
