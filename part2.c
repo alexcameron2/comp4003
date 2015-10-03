545ee25cc51ba409289f52387c84d681547f66a4
alexcameron2 <onboardrope@gmail.com>
2015-10-03 16:53:00 -0400

    mandatory message

--- part2.c
+++ part2.c
@@ -3,14 +3,25 @@
 #include <stdio.h>
 #include <string.h>
 
+exec sql include sqlca;
+	
+exec sql begin declare section;
+	char *identity = "oracle/oracle11g";
+	char statement[1024];
+exec sql end declare section;
+
+
 void catchOracleError();
 
 int main(){
-	exec sql begin declare section;
-		char *user = "oracle";
-		char pword = "oracle11g";
-	exec sql end declare section;
-
-	printf("Connected to Oracle\n");
-return;	 
+	exec sql connect :identity;
+	if (sqlca.sqlcode < 0){
+		printf("Could not connect to oracle!\n");
+		exit(1);
+	}
+	else{
+		printf("Connected to Oracle\n");
+	}
+	
+exit(1);	 
 }
